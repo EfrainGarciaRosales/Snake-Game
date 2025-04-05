49a00837f962d1b16a5679019ac7cd825d70b02b
Efrain Rosales <efraingarciarosales@outlook.com>
2025-04-05 17:58:15 -0400

    Adding snake and food block size pt 1

--- main.py
+++ main.py
@@ -15,3 +15,27 @@ white = (255, 255, 255)
 black = (0,0,0)
 red = (255, 0, 0)
 green = (0, 255, 0)
+
+# Snake and food block size
+block_size = 20
+clock = pygame.time.Clock()
+font = pygame.font.SysFont(none, 35)
+
+def draw_snake(block_size, snake_list):
+    for block in snake_list:
+        pygame.draw.rect(screen,green,[block[0],block[1],block_size, block_size])
+
+def message(msg, color):
+    text = font.render(msg, True, color)
+    screen.blit(text, [width / 6, height / 3])
+
+def game_loop ():
+    game_over = False
+    game_close = False
+    x = width //2
+    y = height //2
+    x_change = 0
+    y_change = 0
+
+    snake_list = []
+    length_of_snake = 1
